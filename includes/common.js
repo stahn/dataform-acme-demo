9d514c0245b8b1e7e6f7e85faf5637d2db06843a
Stanislaw Magierski <google-oauth2|105190414135458061629>
2021-03-21 00:36:50 +0000

    break things

--- includes/common.js
+++ includes/common.js
@@ -5,12 +5,7 @@
 
 function ageBucket(age) {
   const age_clean = `cast(${age} as int64)`;
-  return `case
-            when ${age_clean} between 0 and 18 then "Under 18"
-            when ${age_clean} between 19 and 35 then "19 to 35"
-            when ${age_clean} between 36 and 50 then "36 to 50"
-            else "50+"
-          end`;
+  return age_clean;
 }
 
 module.exports = {
